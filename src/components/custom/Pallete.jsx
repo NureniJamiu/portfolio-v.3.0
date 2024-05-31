a4508674043ddb891e52ac132d346afcf8a1950d
NureniJamiu <nurenijamiu951@gmail.com>
2024-05-31 09:01:19 +0100

    add pallete component

---
+++ src/components/custom/Pallete.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Pallete = () => {
+  return (
+    <div className="flex items-center gap-[1px]">
+      <div className="w-11 h-[5px] bg-[#F3F3F3]"></div>
+      <div className="w-6 h-[5px] bg-[#F3F3F3]"></div>
+      <div className="w-6 h-[5px] bg-[#B8B8B8]"></div>
+      <div className="w-6 h-[5px] bg-[#9B9B9B]"></div>
+      <div className="w-6 h-[5px] bg-[#727272]"></div>
+      <div className="w-6 h-[5px] bg-[#2B2B2B]"></div>
+      <div className="w-6 h-[5px] bg-[#202020]"></div>
+    </div>
+  );
+};
+
+export default Pallete;
