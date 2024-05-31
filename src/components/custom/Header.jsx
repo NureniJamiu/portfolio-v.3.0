67cd9cdef28f0db8d3f7d2d3455ce9834d285b1d
NureniJamiu <nurenijamiu951@gmail.com>
2024-05-31 10:30:40 +0100

    add theme switcher to header

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import Pallete from "./Pallete";
 import Divider from "./Divider";
 import Link from "next/link";
+import ThemeSwitcher from "./ThemeSwitcher";
 
 const Header = () => {
   return (
     <>
       <Pallete />
       <div className="my-4">
-        <div>
+        <div className="flex items-center gap-3">
           <Link href="/" className="font-semibold text-[15px]">
             Nureni O. Jamiu
           </Link>
+          <ThemeSwitcher />
         </div>
         <span className="text-[14px] text-[#727272]">
           Web Developer at BoughView Innovations
