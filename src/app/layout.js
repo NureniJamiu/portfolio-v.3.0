96a3bf3faae58d25451ff4eee68d3270a056ba33
NureniJamiu <nurenijamiu951@gmail.com>
2024-05-31 10:29:29 +0100

    configure new layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,14 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import "primereact/resources/themes/lara-light-cyan/theme.css";
-import DockComponent from "@/components/custom/DockComponent";
 import Header from "@/components/custom/Header";
+
+// primereact config
+import { PrimeReactProvider } from "primereact/api";
+import Tailwind from "primereact/passthrough/tailwind";
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+import "primeicons/primeicons.css";
+import { Dock, DockIcon } from "@/components/magicui/dock";
+import { DockComponent } from "@/components/custom/DockComponent";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,14 +19,16 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${inter.className} bg-[#151515] text-white px-4 max-w-4xl mx-auto`}
-      >
-        <Header />
-        {children}
-        <DockComponent />
-      </body>
-    </html>
+    <PrimeReactProvider value={{ unstyled: true, pt: Tailwind }}>
+      <html lang="en">
+        <body
+          className={`${inter.className} relative bg-[#F3F3F3] dark:bg-[#151515] dark:text-white px-4 max-w-4xl mx-auto`}
+        >
+          <Header />
+          {children}
+          <DockComponent />
+        </body>
+      </html>
+    </PrimeReactProvider>
   );
 }
