69425c494470e1ac31b98f7dc9f524d533e63c67
NureniJamiu <nurenijamiu951@gmail.com>
2024-05-31 17:51:46 +0100

    add page title

--- src/app/bookmarks/page.jsx
+++ src/app/bookmarks/page.jsx
@@ -1,7 +1,15 @@
+import PageTitle from "@/components/custom/PageTitle";
 import React from "react";
 
 const Page = () => {
-  return <div>Bookmarks</div>;
+  return (
+    <div>
+      <PageTitle
+        title="Bookmarks"
+        description="Archive of interesting links."
+      />
+    </div>
+  );
 };
 
 export default Page;
