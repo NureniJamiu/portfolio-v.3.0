5c6ce4ccd64ad3b2237287891b9814d3ab3341d8
NureniJamiu <nurenijamiu951@gmail.com>
2024-05-31 09:00:49 +0100

    reset to empty page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,3 @@
-import DockComponent from "@/components/custom/DockComponent";
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2 className="text-7xl">Nureni Jamiu</h2>
-    </main>
-  );
+  return <main className="flex min-h-screen flex-col "></main>;
 }
