4058da6cf2dc8d5dda9ac997b720b1d5d298d7b3
NureniJamiu <nurenijamiu951@gmail.com>
2024-05-31 09:00:34 +0100

    add header and dock to layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import "primereact/resources/themes/lara-light-cyan/theme.css";
 import DockComponent from "@/components/custom/DockComponent";
+import Header from "@/components/custom/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +14,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-black text-white`}>
+      <body
+        className={`${inter.className} bg-[#151515] text-white px-4 max-w-4xl mx-auto`}
+      >
+        <Header />
         {children}
         <DockComponent />
       </body>
