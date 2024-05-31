ed8b03c2a2bc06787302f23a9db7b5d2bfe356c4
NureniJamiu <nurenijamiu951@gmail.com>
2024-05-31 17:51:10 +0100

    add page title component

---
+++ src/components/custom/PageTitle.jsx
@@ -0,0 +1,22 @@
+import { ArrowDownRight } from "lucide-react";
+import { root } from "postcss";
+import React from "react";
+import Divider from "./Divider";
+
+const PageTitle = ({ title, description }) => {
+  return (
+    <div className="mt-36">
+      <div className="flex items-end gap-1">
+        <h4 className="font-semibold text-[15px]">{title}</h4>
+        <ArrowDownRight size={15} />
+      </div>
+      <p className="text-[14px] text-[#727272]">{description}</p>
+
+      <div className="my-6">
+        <Divider />
+      </div>
+    </div>
+  );
+};
+
+export default PageTitle;
