d8d6a78bb4f1d0412671611e6e7e4a6ee8104942
NureniJamiu <nurenijamiu951@gmail.com>
2024-06-01 14:36:13 +0100

    fix: change video size to aspect ratio video size

--- src/components/custom/WelcomeVid.jsx
+++ src/components/custom/WelcomeVid.jsx
@@ -4,7 +4,7 @@ import { Video } from "lucide-react";
 
 const WelcomeVid = () => {
   return (
-    <ShineBorder className="h-96 w-full bg-[#B8B8B8] text-center text-2xl font-bold capitalize border-red-900 dark:border-white">
+    <ShineBorder className="aspect-video w-full bg-[#B8B8B8] text-center text-2xl font-bold capitalize border-red-900 dark:border-white">
       <div className="flex items-center justify-center ">
         <Video size={85} className="text-[#727272]" />
       </div>
