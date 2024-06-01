b9282b98d4c1060c793db361ca626616b70190a8
NureniJamiu <nurenijamiu951@gmail.com>
2024-06-01 14:31:40 +0100

    feat: add custom markdown component

---
+++ src/components/custom/MarkdownComponent.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Markdown from "react-markdown";
+import rehypeRaw from "rehype-raw";
+import remarkGfm from "remark-gfm";
+
+const MarkdownComponent = ({ children }) => {
+  return (
+    <div>
+      <Markdown
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[rehypeRaw]}
+        components={{
+          a(props) {
+            const { node, ...rest } = props;
+            return (
+              <a
+                {...rest}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="border-b-[1.5px] border-b-[#727272] hover:border-b-[#673de6]"
+              />
+            );
+          },
+          blockquote(props) {
+            const { node, ...rest } = props;
+            return (
+              <blockquote
+                {...rest}
+                className="ml-3 border-l-2 border-[#727272] pl-5"
+              />
+            );
+          },
+        }}
+      >
+        {children}
+      </Markdown>
+    </div>
+  );
+};
+
+export default MarkdownComponent;
