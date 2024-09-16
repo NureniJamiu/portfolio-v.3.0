6c1e1081eb8d51b65008577e4ce5996f4c8ac879
NureniJamiu <nurenijamiu951@gmail.com>
2024-09-16 14:26:20 +0100

    configure middleware

--- src/utils/supabase/middleware.js
+++ src/utils/supabase/middleware.js
@@ -35,16 +35,37 @@ export async function updateSession(request) {
     data: { user },
   } = await supabase.auth.getUser()
 
-  if (
+   // If user is not authenticated and trying to visit /dashboard or /dashboard/*, redirect to /login
+   if (
     !user &&
-    !request.nextUrl.pathname.startsWith('/login') &&
-    !request.nextUrl.pathname.startsWith('/auth')
+    (request.nextUrl.pathname === '/dashboard' ||
+     request.nextUrl.pathname.startsWith('/dashboard/'))
   ) {
-    // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone()
     url.pathname = '/login'
     return NextResponse.redirect(url)
   }
+
+  // If user is authenticated and trying to visit the /login page, redirect to home (/dashboard)
+  if (user && request.nextUrl.pathname.startsWith('/login')) {
+    const url = request.nextUrl.clone()
+    url.pathname = '/dashboard'
+    return NextResponse.redirect(url)
+  }
+
+  // If user is not authenticated and not trying to visit /login or /auth, redirect to /login
+  if (
+    !user &&
+    !request.nextUrl.pathname.startsWith('/login') &&
+    !request.nextUrl.pathname.startsWith('/auth') &&
+    request.nextUrl.pathname !== '/'
+  ) {
+    const url = request.nextUrl.clone()
+    url.pathname = '/'
+    return NextResponse.redirect(url)
+  }
+
+
 
   // IMPORTANT: You *must* return the supabaseResponse object as it is. If you're
   // creating a new response object with NextResponse.next() make sure to:
