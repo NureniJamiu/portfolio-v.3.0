6e8881e5757e2684f02095677e4eef4bc050acfb
NureniJamiu <nurenijamiu951@gmail.com>
2024-09-16 05:26:27 +0100

    fix: prerendering error

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -14,10 +14,10 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import Link from 'next/link'
 import { useRouter, useSearchParams } from 'next/navigation'
-import { useState, Suspense } from 'react'
+import { Suspense, useState } from 'react'
 import { login, signup } from './actions'
 
-const Login = () => {
+const LoginForm = () => {
     const router = useRouter()
     const searchParams = useSearchParams()
 
@@ -31,91 +31,97 @@ const Login = () => {
     const [errors, setErrors] = useState([])
 
     return (
+        <form action={login}>
+            <Card className="rounded-xl">
+                <CardHeader>
+                    <CardTitle className="text-2xl flex items-center justify-between">
+                        Welcome Admin
+                        <ThemeSwitcher />
+                    </CardTitle>
+                    <CardDescription>
+                        Enter your email below to login to your account
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    {error && (
+                        <p className="mb-4 rounded text-center p-1 bg-red-100 text-red-500">
+                            {error}
+                        </p>
+                    )}
+                    {message && (
+                        <p className="mb-4 rounded text-center p-1 bg-green-100 text-green-500">
+                            {message}
+                        </p>
+                    )}
+                    <div className="grid gap-4">
+                        <div className="grid gap-2">
+                            <Label htmlFor="email">Email</Label>
+                            <Input
+                                id="email"
+                                name="email"
+                                type="email"
+                                placeholder="Email..."
+                                value={email}
+                                onChange={event =>
+                                    setEmail(event.target.value)
+                                }
+                                required
+                                autoFocus
+                            />
+                            <InputError
+                                messages={errors.email}
+                                className="mt-2"
+                            />
+                        </div>
+                        <div className="grid gap-2">
+                            <div className="flex items-center">
+                                <Label htmlFor="password">Password</Label>
+                                <Link
+                                    href="#"
+                                    className="ml-auto inline-block text-sm underline">
+                                    Forgot your password?
+                                </Link>
+                            </div>
+                            <Input
+                                id="password"
+                                name="password"
+                                type="password"
+                                onChange={event =>
+                                    setPassword(event.target.value)
+                                }
+                                required
+                                autoComplete="current-password"
+                            />
+                            <InputError
+                                messages={errors.password}
+                                className="mt-2"
+                            />
+                        </div>
+                        <Button
+                            formAction={login}
+                            className="w-full">
+                            Login
+                        </Button>
+                    </div>
+                    <div className="mt-4 text-center text-sm">
+                        Don&apos;t have an account?{' '}
+                        <Button
+                            variant="link"
+                            className="underline"
+                            formAction={signup}>
+                            Sign up
+                        </Button>
+                    </div>
+                </CardContent>
+            </Card>
+        </form>
+    )
+}
+
+const Login = () => {
+    return (
         <Suspense fallback={<div>Loading...</div>}>
-            <form action={login}>
-                <Card className="rounded-xl">
-                    <CardHeader>
-                        <CardTitle className="text-2xl flex items-center justify-between">
-                            Welcome Admin
-                            <ThemeSwitcher />
-                        </CardTitle>
-                        <CardDescription>
-                            Enter your email below to login to your account
-                        </CardDescription>
-                    </CardHeader>
-                    <CardContent>
-                        {error && (
-                            <p className="mb-4 rounded text-center p-1 bg-red-100 text-red-500">
-                                {error}
-                            </p>
-                        )}
-                        {message && (
-                            <p className="mb-4 rounded text-center p-1 bg-green-100 text-green-500">
-                                {message}
-                            </p>
-                        )}
-                        <div className="grid gap-4">
-                            <div className="grid gap-2">
-                                <Label htmlFor="email">Email</Label>
-                                <Input
-                                    id="email"
-                                    name="email"
-                                    type="email"
-                                    placeholder="Email..."
-                                    value={email}
-                                    onChange={event =>
-                                        setEmail(event.target.value)
-                                    }
-                                    required
-                                    autoFocus
-                                />
-                                <InputError
-                                    messages={errors.email}
-                                    className="mt-2"
-                                />
-                            </div>
-                            <div className="grid gap-2">
-                                <div className="flex items-center">
-                                    <Label htmlFor="password">Password</Label>
-                                    <Link
-                                        href="#"
-                                        className="ml-auto inline-block text-sm underline">
-                                        Forgot your password?
-                                    </Link>
-                                </div>
-                                <Input
-                                    id="password"
-                                    name="password"
-                                    type="password"
-                                    onChange={event =>
-                                        setPassword(event.target.value)
-                                    }
-                                    required
-                                    autoComplete="current-password"
-                                />
-                                <InputError
-                                    messages={errors.password}
-                                    className="mt-2"
-                                />
-                            </div>
-                            <Button
-                                formAction={login}
-                                className="w-full">
-                                Login
-                            </Button>
-                        </div>
-                        <div className="mt-4 text-center text-sm">
-                            Don&apos;t have an account?{' '}
-                            <Button
-                                variant="link"
-                                className="underline"
-                                formAction={signup}>
-                                Sign up
-                            </Button>
-                        </div>
-                    </CardContent>
-                </Card>
-            </form>
+            <LoginForm />
         </Suspense>
     )
 }
