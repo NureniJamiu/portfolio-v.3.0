dcef3e5079767fa8a5ddbe5d871c6aeb024b31f0
NureniJamiu <nurenijamiu951@gmail.com>
2025-03-15 09:34:16 -0700

    style: change background color

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,21 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-    title: 'Nureni Jamiu',
-    description: "Nureni Jamiu's portfolio website",
-}
+  title: "Nureni Jamiu",
+  description: "Nureni Jamiu's portfolio website",
+};
 
 export default function RootLayout({ children }) {
-    return (
-        <html lang="en">
-            <body
-                className={`${inter.className} relative bg-[#f3f3f3] dark:bg-[#151515] dark:text-white`}>
-                {children}
-            </body>
-        </html>
-    )
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.className} relative bg-[#f3f3f3] dark:bg-black dark:text-white`}
+      >
+        {children}
+      </body>
+    </html>
+  );
 }
