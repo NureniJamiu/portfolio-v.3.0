b61d29cab9848e3e0f3e6afa28c1fc897d1870a5
NureniJamiu <nurenijamiu951@gmail.com>
2025-03-15 09:30:02 -0700

    adjust layout to contain margin

--- src/app/(app)/layout.js
+++ src/app/(app)/layout.js
@@ -2,10 +2,11 @@ import Header from "@/components/custom/Header";
 
 const layout = ({ children }) => {
   return (
-    <div className="max-w-3xl mx-auto border border-1.5 border-neutral-800">
-      <Header />
-      {children}
-      {/* <DockComponent /> */}
+    <div className="max-w-3xl mx-auto">
+      <div className="border border-1.5 border-neutral-800 mx-4">
+        <Header />
+        {children}
+      </div>
     </div>
   );
 };
