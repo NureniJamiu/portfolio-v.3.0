0c07e390a56955e839c3c796b908593867a4738e
NureniJamiu <nurenijamiu951@gmail.com>
2025-03-15 08:06:02 -0700

    change: hero video

--- src/components/custom/WelcomeVid.jsx
+++ src/components/custom/WelcomeVid.jsx
@@ -7,15 +7,15 @@ const WelcomeVid = () => {
         className="block dark:hidden"
         animationStyle="top-in-bottom-out"
         videoSrc="https://www.youtube.com/embed/XAOuIy2YBZk?si=UL2iAfo7Y_HYOOkW"
-        thumbnailSrc="https://res.cloudinary.com/dasjswerc/image/upload/v1742049480/uploads/t59udbtesma25dlbhfc9.jpg"
-        thumbnailAlt="Hero Video"
+        thumbnailSrc="https://res.cloudinary.com/dasjswerc/image/upload/v1742050907/uploads/w1pvyiter3bnfstitjq9.jpg"
+        thumbnailAlt="Video about me"
       />
       <HeroVideoDialog
         className="hidden dark:block"
         animationStyle="top-in-bottom-out"
         videoSrc="https://www.youtube.com/embed/XAOuIy2YBZk?si=UL2iAfo7Y_HYOOkW"
-        thumbnailSrc="https://res.cloudinary.com/dasjswerc/image/upload/v1742049480/uploads/t59udbtesma25dlbhfc9.jpg"
-        thumbnailAlt="Hero Video"
+        thumbnailSrc="https://res.cloudinary.com/dasjswerc/image/upload/v1742050907/uploads/w1pvyiter3bnfstitjq9.jpg"
+        thumbnailAlt="Video about me"
       />
     </div>
   );
