0bb74579bc43aaaa8093d2481bc595a5e94964f2
NureniJamiu <nurenijamiu951@gmail.com>
2025-03-16 03:23:50 -0700

    style: change border width

--- src/app/(app)/layout.js
+++ src/app/(app)/layout.js
@@ -3,7 +3,7 @@ import Header from "@/components/custom/Header";
 const layout = ({ children }) => {
   return (
     <div className="max-w-3xl mx-auto">
-      <div className="border-[1.5px] border-neutral-800 mx-4">
+      <div className="border border-neutral-800 mx-4">
         <Header />
         {children}
       </div>
