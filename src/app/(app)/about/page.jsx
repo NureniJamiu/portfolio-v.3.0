b64ddb9c1b1a28dd4ee7a65339f1e2aa94930889
NureniJamiu <nurenijamiu951@gmail.com>
2025-03-16 04:15:07 -0700

    dump dummy data into about page

--- src/app/(app)/about/page.jsx
+++ src/app/(app)/about/page.jsx
@@ -13,7 +13,7 @@ const Page = () => {
       <div>
         <div className="flex gap-5 text-[15px] p-4 text-[#2b2b2b] dark:text-neutral-400">
           <div className="mt-2 space-y-4">
-            <p>
+            {/* <p>
               Hello - again. I am a Frontend developer who loves building things
               and helping people. I am currently pursuing a <b>double degree</b>{" "}
               in <span className="font-bold underline">Computer Science</span>{" "}
@@ -27,11 +27,121 @@ const Page = () => {
                 Lagos State university
               </span>
               , Nigeria.
-            </p>
+            </p> */}
+            <p>Hello – again. I promise you, this is gonna be a long read. </p>
 
             <p>
-              That&apos;s me in 10 seconds. If that peaked your interest and
-              would like to know more, then keep scrolling
+              I believe in learning things the long (often harder) way. Like
+              finding the energy to have a website (or whatever this is).
+              I&apos;m not 100% sure it is a wise use of my time but it feels
+              right to speak my truth. I design websites because I got good
+              enough to get paid for it (crazy that I get to feed my family
+              doing this). I probably sound like a basic techie. Perhaps. These
+              few words, and what I do everyday are the only trace of my
+              existence. There is so much stuff out there. It&apos;s at best
+              distracting but realistically destructive (by fragmenting our
+              attention, then intellect and ultimately our society). I
+              don&apos;t want this for myself, my people, and honestly, the
+              world. I crave contemplation but rarely get to it. Discipline and
+              self examination keep me sane (and lots of walks and bike rides).
+            </p>
+            <p>
+              I was born and raised in Nigeria, then in late 2016, Jessica Ko
+              found me on dribbble and flew me to the Bay Area to join
+              Opendoor&apos;s design team where I spent 5 years. In less than 6
+              years I went from living out of a few bags to have a family.
+              It&apos;s been wild to say the least. I never planned any of that.
+              While I&apos;m filled with gratitude I want to put as much
+              intention as I can to figure out where I want to live and what
+              kind of life. That - takes most of my free time. I like to think
+              that I practice a sort of socratic examined life; a wholesome
+              summum bonum. It has been complicated for me to find much
+              satisfaction in my contribution as a web designer. A website is a
+              website. Yet almost ten years later I&apos;m still here, making
+              websites.
+            </p>
+            <p>
+              An odd mix work ethic and ego led me to define myself through the
+              stuff I make. I like to make stuff (almost always too fast). That
+              qualifies for the tech bro archetype, who needs to "make" things
+              (often obvious mistakes) to learn. Unfortunately I am one of those
+              monkeys for whom most learning comes from experience. As I age, I
+              get reminded that the stuff that viscerally gets acknowledged
+              rarely comes from passive consumption but rather from deeply
+              engaged acts. Being a designer has been technical enough to make
+              me believe I understand things better over time. But also abstract
+              to keep me humble and hungry. Images and words are imperfect. It
+              looks like we are just realizing the problem with words while
+              we&apos;ve been aware of how images can distort our perception for
+              a long time. I&apos;ll keep making images and code stuff,
+              that&apos;s my way to chip away at life&apos;s monolith, or to
+              push my rock up the mountain like Sisyphus. I spend most of my
+              time torn between ascetic ideals and pragmatic coping strategies,
+              that&apos;s my version of the examined life.
+            </p>
+            <p>
+              After quitting social media I decided to put more intention in the
+              way I use the internet in general. I used to think about things in
+              terms of cost/benefit only and fell for the potential value
+              without indexing the attention cost. I&apos;m now reclaiming my
+              fragmented attention. The few connections, ideas, and laughs I got
+              from it are not worth the space it took in my head. Of course,
+              I&apos;m afraid of the social stigma and the potential effect it
+              could have on my career... after all, I didn&apos;t delete my
+              Linkedin account. It says something about me. Those who will
+              examine my online presence will perceive this differently – be it.
+              I know the design world is all up on twitter and instagram. Mostly
+              for reach and interaction according to my understanding. Vanity,
+              garbage, and burnout are too much for me to justify the effort of
+              coping. I&apos;ll be active where relevant: Dribbble and this
+              site.
+            </p>
+            <p>
+              I don&apos;t think or care much about tech stack, data,
+              accessibility or AI. A scary sentence to write but that is
+              partially what motivated me to leave social media that seems
+              obsessed with these matters. The web industry is increasingly
+              fragmented and driven by highly specialized tools. This promotes a
+              loss of autonomy by increasing dependencies and lack of (human)
+              specialists. Too many inputs and tools have distracted me from the
+              human reality of the creative process: consider context, take
+              decisions, do the work and iterate. Limiting inputs narrows the
+              context, it&apos;s crucial for me to stay functional. I often
+              choose to center around tangibility and what I judge is the wisest
+              feasible action. That may sound pretentious but it allows me to
+              find meaning in what I do. Data and AI are two polarizing topics.
+              I&apos;m conformably stating that: They both are tools, and they
+              will be leveraged to create the usual 80-20 ratio of trash to
+              goodness. Designers seem perpetually threatened by
+              democratization. Tool after tool, it&apos;s the same story.
+              It&apos;s absurd. My interest lies in creating stuff grounded in
+              good practices that reflect my team&apos;s opinion and best
+              execution while using the simplest tool to make the thing. So far
+              AI has been fun but not particularly insightful and Data has been
+              frustrating as its source and meaning often seem to be here to
+              justify a request more than a real need.
+            </p>
+            <p>
+              While I&apos;m not a technologist, I&apos;m not a monk either.
+              I&apos;m way too fidgety and working on it. Modern tech devices
+              really rub my monkey mind the wrong way but I get that I need to
+              accept it as a part of life. So I&apos;ve been a fair-weather
+              meditator for a while now. Believing that discipline was the only
+              thing keeping me away from the peace and insights (silly, I know)
+              I&apos;ve been practicing on and off for years. Recent ventures
+              into Zen and Daoism thought have removed the goals from the
+              ritual, providing just enough guidance to allow a glance of the
+              feeling. A feeling, not words or any other symbols. Understanding
+              the importance of the non-verbal experience of life has been
+              eye-opening. So when time opens up, I sit, just to sit, relax what
+              is between my eyes, and watch my posture and breath.
+            </p>
+            <p>
+              They say feedback is a gift. While I don&apos;t track anything, I
+              like to know how you feel about being here on my website so feel
+              free to reach out. My gratitude for the opportunity to be here now
+              is larger than an html file can hold. You could be anywhere — and
+              yet, you are here now.
             </p>
           </div>
         </div>
