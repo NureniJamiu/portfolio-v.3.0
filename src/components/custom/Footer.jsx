92212e0989809f798726416d60b7be6c9f2289b6
NureniJamiu <nurenijamiu951@gmail.com>
2025-03-17 05:48:49 -0700

    style: display footer on medium and large screens

--- src/components/custom/Footer.jsx
+++ src/components/custom/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="md:hidden text-center text-[14px] text-[#2b2b2b] dark:text-neutral-300 py-5 md:py-0 md:pt-10">
+    <footer className="text-center text-[14px] text-[#2b2b2b] dark:text-neutral-300 py-5 md:py-0 md:pt-10">
       <p>
         Made with ❤️ by <b>Nureni Jamiu Olawale</b>
       </p>
