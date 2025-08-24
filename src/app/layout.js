f86a965aca01d5a10344a8bc3fe8cb6afd80e9a3
NureniJamiu <nurenijamiu951@gmail.com>
2025-08-24 07:14:29 +0100

    style: change font from Inter to Jetbrains_mono

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,8 @@
-import { Inter } from "next/font/google";
+import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
-const inter = Inter({ subsets: ["latin"] });
+const jetBrainsMono = JetBrains_Mono({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Nureni Jamiu",
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${inter.className} relative bg-[#f3f3f3] dark:bg-black dark:text-white`}
+        className={`${jetBrainsMono.className} relative bg-[#f3f3f3] dark:bg-black dark:text-white`}
       >
         {children}
         <SpeedInsights />
