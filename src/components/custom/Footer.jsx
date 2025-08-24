90211abf5c1151072aa8ba1e0d7aed37b4fe8212
NureniJamiu <nurenijamiu951@gmail.com>
2025-08-24 08:43:01 +0100

    style: add top border to footer component

--- src/components/custom/Footer.jsx
+++ src/components/custom/Footer.jsx
@@ -1,10 +1,10 @@
 const Footer = () => {
   return (
-    <footer className="text-center text-[14px] text-[#2b2b2b] dark:text-neutral-300 py-5 md:py-0 md:pt-10">
-      <p>
-        Made with ❤️ by <b>Nureni Jamiu Olawale</b>
-      </p>
-    </footer>
+      <footer className="text-center text-[14px] text-[#2b2b2b] dark:text-neutral-300 py-5 md:py-0 md:pt-3 border-t border-neutral-800">
+          <p>
+              Made with ❤️ by <b>Nureni Jamiu Olawale</b>
+          </p>
+      </footer>
   );
 };
 
