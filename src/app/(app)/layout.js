0abb5007357de25498e2062d64f7611db46a8075
NureniJamiu <nurenijamiu951@gmail.com>
2025-08-24 09:15:53 +0100

    style: optimize layout for mobile responsiveness

--- src/app/(app)/layout.js
+++ src/app/(app)/layout.js
@@ -8,7 +8,7 @@ const layout = ({ children }) => {
             <Header />
             <div className="relative isolate max-w-[1400px] mx-auto">
                 <PatternBorders />
-                <div className="max-w-[1350px] mx-auto border-t border-neutral-800 px-10">
+                <div className="max-w-[1350px] mx-auto border-t border-neutral-800 md:px-10">
                     {children}
                 </div>
                 <Footer />
